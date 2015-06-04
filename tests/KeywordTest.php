e3c4ba668396eec780a86b79880c73554465795d
neun <nimmneun@gmail.com>
2015-06-05 00:28:59 +0200

    eight

--- tests/KeywordTest.php
+++ tests/KeywordTest.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Keydings;
+namespace Keyding;
 
 class KeywordTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetId()
     {
-        $keyword = new \Keydings\Keyword;
+        $keyword = new \Keyding\Keyword;
         $this->assertExquals('99', $keyword->getId());
     }
 }
