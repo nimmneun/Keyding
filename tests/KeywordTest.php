cfa03a35167d6794d95a4013f3ecb7d30ec3c3a4
Stefan <nimmneun@users.noreply.github.com>
2015-06-05 08:25:19 +0200

    Update KeywordTest.php

--- tests/KeywordTest.php
+++ tests/KeywordTest.php
@@ -7,20 +7,34 @@ class KeywordTest extends \PHPUnit_Framework_TestCase
     public function testGetId()
     {
         $keyword = new \Keyding\Keyword;
-	$keyword->setId(123);
+        $keyword->setId(123);
         $this->assertEquals('123', $keyword->getId());
     }
 
     public function testGetPhrase()
     {
         $keyword = new \Keyding\Keyword;
-	$keyword->setPhrase('phonky widgets');
+        $keyword->setPhrase('phonky widgets');
         $this->assertEquals('phonky widgets', $keyword->getPhrase());
     }
     public function testGetVolume()
     {
         $keyword = new \Keyding\Keyword;
-	$keyword->setVolume(9700);
+        $keyword->setVolume(9700);
         $this->assertEquals(9700, $keyword->getVolume());
+    }
+    
+    public function testChain()
+    {
+        $keyword = new \Keyding\Keyword;
+        
+
+        $this->assertEquals('lazy widgets',
+            $keyword
+            ->setId(321)
+            ->setPhrase('lazy widgets')
+            ->setVolume(11000)
+            ->getPhrase()
+        );
     }
 }
