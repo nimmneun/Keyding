d54d0a5a7564b0e117081d780fc6944dab794fdb
neun <nimmneun@gmail.com>
2015-06-05 23:56:33 +0200

    more validations

--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -11,11 +11,21 @@ class ValidateTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(false, $validate->id(-123));
     }
 
+    public function testNullableId()
+    {
+        $validate = new \Keyding\Validate;
+        $this->assertEquals(true, $validate->nullableId(123));
+        $this->assertEquals(true, $validate->nullableId(null));
+        $this->assertEquals(false, $validate->nullableId(0));
+        $this->assertEquals(false, $validate->nullableId(-123));
+    }
+
     public function testTime()
     {
         $validate = new \Keyding\Validate;
         $this->assertEquals(true, $validate->time('17:15:53'));
-        $this->assertEquals(false, $validate->time(25:14:33));
-        $this->assertEquals(false, $validate->time(22:64:33));
+        $this->assertEquals(true, $validate->time('7:5:03'));
+        $this->assertEquals(false, $validate->time('25:14:33'));
+        $this->assertEquals(false, $validate->time('22:64:33'));
     }
 }
